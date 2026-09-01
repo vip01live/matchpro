2898f0d38c844c243b05f33675c711324a32e418
vip01live <vrurdza@gmail.com>
2026-09-02 00:09:32 +0400

    Simplify MatchPro global advertising loader

--- main/js/ads-loader.js
+++ main/js/ads-loader.js
@@ -3,21 +3,19 @@
   if (window.MATCHPRO_ADS_LOADED) return;
   window.MATCHPRO_ADS_LOADED = true;
 
-  function load(src) {
-    var s = document.createElement('script');
-    s.src = src;
-    s.async = true;
-    s.defer = true;
-    s.dataset.matchproAd = '1';
-    document.head.appendChild(s);
-  }
-
   /*
-   * Put your ad JavaScript files in /main/ads/ and name them ad1.js, ad2.js,
-   * ad3.js ... ad20.js. Every existing file is loaded automatically on every
-   * MatchPro page. Missing files are ignored.
+   * Universal MatchPro advertising slot.
+   * Put your advertising JavaScript code in:
+   *   /main/ads/ad.js
+   *
+   * This file is loaded automatically on every MatchPro page.
+   * You can paste normal JavaScript here or the JavaScript part of an
+   * advertising provider's <script> code.
    */
-  for (var i = 1; i <= 20; i++) {
-    load('/main/ads/ad' + i + '.js');
-  }
+  var s = document.createElement('script');
+  s.src = '/main/ads/ad.js';
+  s.async = true;
+  s.defer = true;
+  s.dataset.matchproAd = '1';
+  document.head.appendChild(s);
 })();
