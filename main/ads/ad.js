165e71033c04b06768afda405fca4b6dbf222d33
vip01live <vrurdza@gmail.com>
2026-09-02 00:09:36 +0400

    Add global MatchPro advertising script folder entry

---
+++ main/ads/ad.js
@@ -0,0 +1,14 @@
+/*
+ * MATCHPRO GLOBAL AD SCRIPT
+ *
+ * Paste your advertising provider's JavaScript code in this file.
+ * It is automatically loaded on every MatchPro page.
+ *
+ * Example:
+ * (function () {
+ *   var s = document.createElement('script');
+ *   s.src = 'https://example.com/ad.js';
+ *   s.async = true;
+ *   document.head.appendChild(s);
+ * })();
+ */
