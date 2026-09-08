fc65cd1821aed00322665568500b6f25d42923fb
vip01live <vrurdza@gmail.com>
2026-09-08 23:17:33 +0400

    Load player-only click redirect on channel pages

--- main/js/player-config.js
+++ main/js/player-config.js
@@ -3,3 +3,14 @@ window.MATCHPRO_PLAYER_CONFIG = {
   oncePerDay: true,
   openInNewTab: true
 };
+
+(function () {
+  'use strict';
+  if (window.MATCHPRO_PLAYER_CLICK_REDIRECT_LOADER) return;
+  window.MATCHPRO_PLAYER_CLICK_REDIRECT_LOADER = true;
+
+  var s = document.createElement('script');
+  s.src = '/main/js/player-click-redirect.js';
+  s.defer = true;
+  document.head.appendChild(s);
+})();
