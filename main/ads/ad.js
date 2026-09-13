4a7bafd5df877b5e0c747f6f6f14ae27f9b181b0
vip01live <vrurdza@gmail.com>
2026-09-13 16:23:41 +0400

    Stabilize MatchPro ad provider injection

--- main/ads/ad.js
+++ main/ads/ad.js
@@ -1,9 +1,7 @@
 /*
  * MATCHPRO GLOBAL AD SCRIPT
  *
- * This single file is loaded by /main/js/ads-loader.js on MatchPro pages.
- * The ad provider scripts are injected into <head> from here, so they do
- * not need to be copied into every HTML page.
+ * Loads the configured advertising providers once on every MatchPro page.
  */
 (function () {
   'use strict';
@@ -18,14 +16,9 @@
       name: 'matchpro-ad-1'
     },
     {
-      zone: '11711828',
-      src: 'https://n6wxm.com/vignette.min.js',
-      name: 'matchpro-ad-2'
-    },
-    {
       zone: '11711877',
       src: 'https://nap5k.com/tag.min.js',
-      name: 'matchpro-ad-3'
+      name: 'matchpro-ad-2'
     }
   ];
 
@@ -37,8 +30,8 @@
       if (document.querySelector('script[data-matchpro-ad="' + ad.name + '"]')) return;
 
       var script = document.createElement('script');
-      script.dataset.zone = ad.zone;
-      script.dataset.matchproAd = ad.name;
+      script.setAttribute('data-zone', ad.zone);
+      script.setAttribute('data-matchpro-ad', ad.name);
       script.src = ad.src;
       script.async = true;
       head.appendChild(script);
